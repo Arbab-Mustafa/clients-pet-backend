3720da52a05f5aad44d39912a36be57fdc459e5b
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-16 22:23:48 +0500

    first commit

--- src/server.js
+++ src/server.js
@@ -11,8 +11,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "http://localhost:5173", // ✅ Allow requests from frontend
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true, // ✅ Allow cookies/auth headers
   })
 );
 app.use(express.json()); // Middleware to parse JSON
